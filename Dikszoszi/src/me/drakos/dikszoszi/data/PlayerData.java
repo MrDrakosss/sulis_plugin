138da61b71d51d7ee3f75679521b39fa32b72ede
MrDrakosss <markoska1231@gmail.com>
2021-10-07 09:15:26 +0200

    asd

---
+++ Dikszoszi/src/me/drakos/dikszoszi/data/PlayerData.java
@@ -0,0 +1,11 @@
+package me.drakos.dikszoszi.data;
+
+public class PlayerData {
+
+    public enum PlayerClass {
+        WARRIOR,
+        MAGE,
+        ARCHER,
+        UNKNOWN
+    }
+}
---
+++ Dikszoszi/src/me/drakos/dikszoszi/mission_data/MissionData.java
@@ -0,0 +1,11 @@
+package me.drakos.dikszoszi.mission_data;
+
+public class MissionData {
+
+    public enum MissionType {
+        MINER,
+        ARCHER,
+        WARRIOR,
+        LUMBERJACK
+    }
+}
